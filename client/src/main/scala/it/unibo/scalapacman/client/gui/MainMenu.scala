760a85b7b797d8614d5f53d938cecde743c334c5
MooDySmiles <diphlo.powa@gmail.com>
2020-06-30 18:11:13 +0200

    Crea pannello menù principale (wip)

---
+++ client/src/main/scala/it/unibo/scalapacman/client/gui/MainMenu.scala
@@ -0,0 +1,47 @@
+package it.unibo.scalapacman.client.gui
+
+import java.awt.{Color, Font}
+
+import it.unibo.scalapacman.client.gui.Panel.PanelImpl
+import javax.swing.{BorderFactory, JButton, JLabel, SwingConstants}
+
+class MainMenu extends PanelImpl {
+  private final val LABEL_X = 180
+  private final val LABEL_Y = 105
+  private final val LABEL_HEIGHT = 300
+  private final val LABEL_WIDTH = 400
+  private final val LABEL_FONT_SIZE = 86
+  private final val BUTTON_X = 270
+  private final val BUTTON_Y = 505
+  private final val BUTTON_Y_OFFSET = 100
+  private final val BUTTON_HEIGHT = 75
+  private final val BUTTON_WIDTH = 150
+  private final val BUTTON_FONT_SIZE = 32
+  private final val FONT_NAME = "Arial"
+
+  private final val title: JLabel = createLabel("<html><div style='text-align: center'>Scala<br>Pacman<div></html>", LABEL_WIDTH, LABEL_HEIGHT)
+
+  title setLocation (LABEL_X, LABEL_Y)
+  title setForeground Color.yellow
+  title setFont new Font(FONT_NAME, Font.BOLD, LABEL_FONT_SIZE)
+
+  private final val playButton: JButton = createButton("Gioca", BUTTON_WIDTH, BUTTON_HEIGHT)
+
+  playButton setLocation (BUTTON_X, BUTTON_Y)
+  playButton setForeground Color.white
+  playButton setFont new Font(FONT_NAME, Font.PLAIN, BUTTON_FONT_SIZE)
+
+  private final val exitButton: JButton = createButton("Esci", BUTTON_WIDTH, BUTTON_HEIGHT)
+
+  exitButton setLocation (BUTTON_X, BUTTON_Y + BUTTON_Y_OFFSET)
+  exitButton setForeground Color.white
+  exitButton addActionListener (_ => System exit 0)
+  exitButton setFont new Font(FONT_NAME, Font.PLAIN, BUTTON_FONT_SIZE)
+
+  //noinspection ScalaStyle
+  setLayout(null)
+  setBackground(Color.black)
+  add(title)
+  add(playButton)
+  add(exitButton)
+}
