1ad641848314864c0e208f1955034a15419ec5e3
MooDySmiles <diphlo.powa@gmail.com>
2020-07-02 18:42:22 +0200

    Modifica layout MainMenu

--- client/src/main/scala/it/unibo/scalapacman/client/gui/MainMenu.scala
+++ client/src/main/scala/it/unibo/scalapacman/client/gui/MainMenu.scala
@@ -1,38 +1,59 @@
 package it.unibo.scalapacman.client.gui
 
-import java.awt.{Color, Font}
-import javax.swing.{JButton, JLabel}
+import java.awt.{BorderLayout, Color, Component, Dimension, Font}
+
+import javax.swing.{Box, BoxLayout, JButton, JLabel, JSplitPane, SwingConstants}
 
 class MainMenu extends PanelImpl {
-  private val LABEL_X = 180
-  private val LABEL_Y = 105
-  private val LABEL_HEIGHT = 300
-  private val LABEL_WIDTH = 400
   private val LABEL_FONT_SIZE = 86
-  private val BUTTON_X = 270
-  private val BUTTON_Y = 505
-  private val BUTTON_Y_OFFSET = 100
-  private val BUTTON_HEIGHT = 75
-  private val BUTTON_WIDTH = 150
+  private val BUTTONS_HORIZONTAL_SPACE = 0
+  private val BUTTONS_VERTICAL_SPACE = 50
+  private val BUTTONS_TOP_SPACE = 50
 
-  private val title: JLabel = createLabel("<html><div style='text-align: center'>Scala<br>Pacman<div></html>", LABEL_WIDTH, LABEL_HEIGHT)
+  private val titleLabel: JLabel = createLabel("<html><div style='text-align: center'>Scala<br>Pacman<div></html>")
+  private val playButton: JButton = createButton("Gioca")
+  private val optionsButton: JButton = createButton("Opzioni")
+  private val statsButton: JButton = createButton("Statistiche")
+  private val exitButton: JButton = createButton("Esci")
 
-  title setLocation (LABEL_X, LABEL_Y)
-  title setForeground Color.yellow
-  title setFont new Font(FONT_NAME, Font.BOLD, LABEL_FONT_SIZE)
+  titleLabel setForeground Color.yellow
+  titleLabel setFont new Font(FONT_NAME, Font.BOLD, LABEL_FONT_SIZE)
+  titleLabel setHorizontalAlignment SwingConstants.CENTER
 
-  private val playButton: JButton = createButton("Gioca", BUTTON_WIDTH, BUTTON_HEIGHT)
+  playButton setAlignmentX Component.CENTER_ALIGNMENT
+  optionsButton setAlignmentX Component.CENTER_ALIGNMENT
+  statsButton setAlignmentX Component.CENTER_ALIGNMENT
+  exitButton setAlignmentX Component.CENTER_ALIGNMENT
 
-  playButton setLocation (BUTTON_X, BUTTON_Y)
-
-  private val exitButton: JButton = createButton("Esci", BUTTON_WIDTH, BUTTON_HEIGHT)
-
-  exitButton setLocation (BUTTON_X, BUTTON_Y + BUTTON_Y_OFFSET)
   exitButton addActionListener (_ => System exit 0)
 
-  //noinspection ScalaStyle
-  setLayout(null)
-  add(title)
-  add(playButton)
-  add(exitButton)
+  private val splitPane = new JSplitPane
+  private val titlePanel = new PanelImpl
+  private val buttonsPanel = new PanelImpl
+
+  splitPane setOrientation JSplitPane.VERTICAL_SPLIT
+  splitPane setDividerLocation 300
+  splitPane setDividerSize 0
+  splitPane setEnabled false
+  splitPane setTopComponent titlePanel
+  splitPane setBottomComponent buttonsPanel
+
+  buttonsPanel setLayout new BoxLayout(buttonsPanel, BoxLayout.Y_AXIS)
+  titlePanel setLayout new BorderLayout
+
+  buttonsPanel add createRigidArea(BUTTONS_HORIZONTAL_SPACE, BUTTONS_TOP_SPACE)
+  buttonsPanel add playButton
+  buttonsPanel add createRigidArea(BUTTONS_HORIZONTAL_SPACE, BUTTONS_VERTICAL_SPACE)
+  buttonsPanel add optionsButton
+  buttonsPanel add createRigidArea(BUTTONS_HORIZONTAL_SPACE, BUTTONS_VERTICAL_SPACE)
+  buttonsPanel add statsButton
+  buttonsPanel add createRigidArea(BUTTONS_HORIZONTAL_SPACE, BUTTONS_VERTICAL_SPACE)
+  buttonsPanel add exitButton
+
+  titlePanel add (titleLabel, BorderLayout.CENTER)
+
+  setLayout(new BorderLayout)
+  add(splitPane)
+
+  private def createRigidArea(horizontalArea: Int, verticalArea: Int): Component = Box.createRigidArea(new Dimension(horizontalArea, verticalArea))
 }
