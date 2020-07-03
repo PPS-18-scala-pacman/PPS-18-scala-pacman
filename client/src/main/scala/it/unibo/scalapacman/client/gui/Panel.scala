81ffd8016527a251c61a986b6e21214b7c788cac
MooDySmiles <diphlo.powa@gmail.com>
2020-07-03 14:42:21 +0200

    Aggiunge funzioni per creazione label aggiuntive

--- client/src/main/scala/it/unibo/scalapacman/client/gui/Panel.scala
+++ client/src/main/scala/it/unibo/scalapacman/client/gui/Panel.scala
@@ -6,6 +6,8 @@ import javax.swing.{JButton, JLabel, JPanel}
 trait Panel {
   def createLabel(text: String): JLabel
   def createLabel(text: String, width: Int, height: Int): JLabel
+  def createMainTitleLabel(text: String): JLabel
+  def createTitleLabel(text: String): JLabel
 
   def createButton(text: String): JButton
   def createButton(text: String, width: Int, height: Int): JButton
@@ -13,6 +15,8 @@ trait Panel {
 
 class PanelImpl extends JPanel with Panel {
   protected final val BUTTON_FONT_SIZE = 32
+  private val LABEL_MAIN_TITLE_FONT_SIZE = 86
+  private val LABEL_TITLE_FONT_SIZE = 56
   protected final val FONT_NAME = "Arial"
 
   setBackground(Color.black)
@@ -26,19 +30,31 @@ class PanelImpl extends JPanel with Panel {
     setCursor(new Cursor(Cursor.HAND_CURSOR))
   }
 
-  override def createButton(text: String, width: Int, height: Int): JButton = new JButton(text) {
-    setFocusPainted(false)
-//    setBorderPainted(false)
-    setContentAreaFilled(false)
-    setForeground(Color.white)
-    setFont(new Font(FONT_NAME, Font.PLAIN, BUTTON_FONT_SIZE))
-    setSize(width, height)
-    setCursor(new Cursor(Cursor.HAND_CURSOR))
+  override def createButton(text: String, width: Int, height: Int): JButton = {
+    val button = createButton(text)
+    button setSize(width, height)
+    button
   }
 
-  override def createLabel(text: String): JLabel = new JLabel(text)
+  override def createLabel(text: String): JLabel = new JLabel(text) {
+    setForeground(Color.white)
+  }
 
-  override def createLabel(text: String, width: Int, height: Int): JLabel = new JLabel(text) {
-    setSize(width, height)
+  override def createLabel(text: String, width: Int, height: Int): JLabel = {
+    val label = createLabel(text)
+    label setSize(width, height)
+    label
+  }
+
+  override def createMainTitleLabel(text: String): JLabel = {
+    val label = createLabel(text)
+    label setFont new Font(FONT_NAME, Font.BOLD, LABEL_MAIN_TITLE_FONT_SIZE)
+    label
+  }
+
+  override def createTitleLabel(text: String): JLabel = {
+    val label = createLabel(text)
+    label setFont new Font(FONT_NAME, Font.BOLD, LABEL_TITLE_FONT_SIZE)
+    label
   }
 }
