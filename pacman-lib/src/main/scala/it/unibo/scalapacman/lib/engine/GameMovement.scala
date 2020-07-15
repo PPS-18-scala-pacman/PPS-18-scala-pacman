cac23afff6e818b27f65797364034ce52ced43bb
McGiogen <mcgiogen@hotmail.it>
2020-07-15 19:18:18 +0200

    Aggiorna la documentazione e rimuove una funzione non utilizzata

--- pacman-lib/src/main/scala/it/unibo/scalapacman/lib/engine/GameMovement.scala
+++ pacman-lib/src/main/scala/it/unibo/scalapacman/lib/engine/GameMovement.scala
@@ -9,18 +9,26 @@ import it.unibo.scalapacman.lib.model.Direction.{EAST, NORTH, SOUTH, WEST}
 object GameMovement {
 
   /**
-   * 1. Desidero invertire la direzione?
-   * - se SI Inverto la rotta
-   * 2. Sono al centro della tile?
-   * - se NO Mi muovo fino al centro (jump 2)
-   * 3. Voglio curvare e la prossima Tile è agibile?
-   * - se SI Cambio la direzione
-   * 4. La prossima tile è agibile?
-   * - se SI finisco di muovermi
+   * Funzione di movimento del personaggio.
+   *
+   * Di seguito l'algoritmo implementato:
+   * 1. Il personaggio ha terminato il tempo a disposizione per muoversi?
+   * -> Termina il movimento
+   * -> Riparte dal punto 1
+   * 2. Il personaggio desidera invertire la direzione?
+   * -> Inverte la direzione
+   * 3. Il personaggio si trova al centro della tile corrente?
+   * -> Se lo desidera ed è possibile, curva cambiando direzione
+   * -> Se la prossima tile è camminabile dal personaggio, si sposta per tutto il tempo rimasto a disposizione
+   * 4. Il personaggio non possiede abbastanza tempo da raggiungere il prossimo centro di una tile?
+   * -> Si sposta per tutto il tempo rimasto a disposizione
+   * 5. Altrimenti il personaggio è in grado di raggiungere il prossimo centro senza consumare tutto il tempo a disposizione
+   * -> Si sposta fino al successivo centro di una tile
+   * -> Riparte dal punto 1
    *
    * @param character        Character to move
    * @param timeMs           Time available
-   * @param desiredDirection Direction to turn, if possible
+   * @param desiredDirection Desired direction, to change if and when possible
    * @param map              Map of the game
    * @return The updated character
    */
