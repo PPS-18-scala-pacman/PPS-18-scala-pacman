5cd63575974c616ca7546fb0adf5973dac4e6313
McGiogen <mcgiogen@hotmail.it>
2020-07-20 00:51:15 +0200

    Implementa funzione per calcolare il punteggio

--- pacman-lib/src/main/scala/it/unibo/scalapacman/lib/engine/GameTick.scala
+++ pacman-lib/src/main/scala/it/unibo/scalapacman/lib/engine/GameTick.scala
@@ -1,17 +1,16 @@
 package it.unibo.scalapacman.lib.engine
 
-import it.unibo.scalapacman.lib.model.{Character, GameObject, Map, Pacman}
+import it.unibo.scalapacman.lib.model.{Character, Eatable, GameObject, GameState, Map, Pacman}
 import it.unibo.scalapacman.lib.engine.GameHelpers.CharacterHelper
 
-object GameCollision {
+object GameTick {
   def collisions(characters: List[Character])(implicit map: Map): List[GameObject] =
-    characters.filter(isPacman).flatMap(characterCollisions(characters))
+    characters collect { case p: Pacman => p } flatMap characterCollisions(characters)
 
   private def characterCollisions(characters: List[Character])(character: Character)(implicit map: Map): List[GameObject] =
     character.tile.eatable ++: characters.filter(_ != character).filter(_.tile eq character.tile)
 
-  private def isPacman(character: Character): Boolean = character match {
-    case Pacman(_, _, _) => true
-    case _ => false
-  }
+  def calculateGameState(collisions: List[GameObject]): GameState = GameState(
+    points = collisions collect { case e: Eatable => e } map (_.points) sum
+  )
 }
