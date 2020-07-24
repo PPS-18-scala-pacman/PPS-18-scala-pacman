4c2716f001dab92df9da5574338e33a18df2e7e2
MooDySmiles <diphlo.powa@gmail.com>
2020-07-24 17:34:19 +0200

    Non gestisce più il cambio schermata, delegato interamente alla GUI. PacmanRestClient viene passato come dipendenza

--- client/src/main/scala/it/unibo/scalapacman/client/controller/Action.scala
+++ client/src/main/scala/it/unibo/scalapacman/client/controller/Action.scala
@@ -9,7 +9,6 @@ object Action {
   case object END_GAME extends UserIntention
   case object SUBSCRIBE_TO_GAME_UPDATES extends UserIntention
   case object EXIT_APP extends UserIntention
-  case object CHANGE_VIEW extends UserIntention
   case object SAVE_KEY_MAP extends UserIntention
   case object RESET_KEY_MAP extends UserIntention
   case object MOVEMENT extends UserIntention
