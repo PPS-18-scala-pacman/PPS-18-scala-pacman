f4d0a5c7a941d43cb48fa91296bbc208d49ba326
luca <luca.maestri@studio.unibo.it>
2020-07-26 21:00:38 +0200

    Aggiunto test per Engine

---
+++ server/src/test/scala/it/unibo/scalapacman/server/core/EngineUpdateTest.scala
@@ -0,0 +1,30 @@
+package it.unibo.scalapacman.server.core;
+
+import akka.actor.testkit.typed.scaladsl.{ScalaTestWithActorTestKit, TestProbe}
+import akka.actor.typed.ActorRef
+import it.unibo.scalapacman.server.util.Settings
+import org.scalatest.wordspec.AnyWordSpecLike
+
+class EngineUpdateTest extends ScalaTestWithActorTestKit with AnyWordSpecLike {
+
+  val fakeGameId = "fakeCreateGameId"
+
+  private var engineActor: ActorRef[Engine.EngineCommand] = _
+  private var watcherPlayerProbe: TestProbe[Engine.UpdateCommand] = _
+
+  override def beforeAll(): Unit = {
+    engineActor = spawn(Engine(fakeGameId))
+    watcherPlayerProbe = createTestProbe[Engine.UpdateCommand]()
+  }
+
+  "An Engine actor" must {
+    "update watcher after registration" in {
+      engineActor ! Engine.RegisterPlayer(watcherPlayerProbe.ref)
+
+      watcherPlayerProbe.receiveMessage(Settings.gameRefreshRate * 2) match {
+        case Engine.UpdateMsg(_) =>
+        case _ => fail()
+      }
+    }
+  }
+}
