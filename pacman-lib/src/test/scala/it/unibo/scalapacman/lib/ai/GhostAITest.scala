2f789dba108804722db5d535f9da0023dccbb298
McGiogen <mcgiogen@hotmail.it>
2020-07-28 19:22:58 +0200

    Implementa funzione per calcolare la direzione desiderata dei fantasmi

--- pacman-lib/src/test/scala/it/unibo/scalapacman/lib/ai/GhostAITest.scala
+++ pacman-lib/src/test/scala/it/unibo/scalapacman/lib/ai/GhostAITest.scala
@@ -1,6 +1,6 @@
 package it.unibo.scalapacman.lib.ai
 
-import it.unibo.scalapacman.lib.model.{Direction, Map, Pacman}
+import it.unibo.scalapacman.lib.model.{Direction, Ghost, Map, Pacman}
 import it.unibo.scalapacman.lib.ai.GhostAI.engine
 import it.unibo.scalapacman.lib.math.{Point2D, TileGeography}
 import it.unibo.scalapacman.lib.model.Map.{emptyTrack, wall}
@@ -21,15 +21,37 @@ class GhostAITest extends AnyWordSpec {
   "Ghost AI" should {
     "calculate the shortest path" when {
       "target tile is next to starting one" in {
-        assert(GhostAI.shortestPath(Pacman(ORIGIN, 1.0, Direction.WEST), (2, 1)) == (1, 1) :: (2, 1) :: Nil)
+        assert(GhostAI.shortestPath(Ghost.clyde(ORIGIN, 1.0, Direction.WEST), (2, 1)) == (1, 1) :: (2, 1) :: Nil)
       }
       "target tile is after the tunnel" in {
-        val path = GhostAI.shortestPath(Pacman(ORIGIN + Point2D(0, TileGeography.SIZE), 1.0, Direction.WEST), (3, 2))
+        val path = GhostAI.shortestPath(Ghost.pinky(ORIGIN + Point2D(0, TileGeography.SIZE), 1.0, Direction.WEST), (3, 2))
         assert(path == (1, 2) :: (0, 2) :: (4, 2) :: (3, 2) :: Nil)
       }
       "target tile is behind a wall" in {
-        val path = GhostAI.shortestPath(Pacman(ORIGIN + Point2D(TileGeography.SIZE, 0), 1.0, Direction.WEST), (2, 3))
+        val path = GhostAI.shortestPath(Ghost.clyde(ORIGIN + Point2D(TileGeography.SIZE, 0), 1.0, Direction.WEST), (2, 3))
         assert(path == (2, 1) :: (3, 1) :: (3, 2) :: (3, 3) :: (2, 1) :: Nil || path == (2, 1) :: (1, 1) :: (1, 2) :: (1, 3) :: (2, 3) :: Nil)
+      }
+    }
+    "calculate the desired direction" when {
+      "target is in front of the ghost" in {
+        var blinky = Ghost.blinky(ORIGIN, 1.0, Direction.NORTH)
+        var pacman = Pacman(ORIGIN + Point2D(0, TileGeography.SIZE), 1.0, Direction.SOUTH)
+        assert(GhostAI.desiredDirection(blinky, pacman) == Direction.SOUTH)
+
+        blinky = Ghost.blinky(ORIGIN + Point2D(0, TileGeography.SIZE), 1.0, Direction.NORTH)
+        pacman = Pacman(ORIGIN, 1.0, Direction.SOUTH)
+        assert(GhostAI.desiredDirection(blinky, pacman) == Direction.NORTH)
+      }
+      "target is after the tunnel" in {
+        val blinky = Ghost.blinky(ORIGIN + Point2D(0, TileGeography.SIZE), 1.0, Direction.EAST)
+        val pacman = Pacman(ORIGIN + Point2D(TileGeography.SIZE * 2, TileGeography.SIZE), 1.0, Direction.WEST)
+        assert(GhostAI.desiredDirection(blinky, pacman) == Direction.WEST)
+      }
+      "target is behind a wall" in {
+        val blinky = Ghost.blinky(ORIGIN + Point2D(TileGeography.SIZE, 0), 1.0, Direction.EAST)
+        val pacman = Pacman(ORIGIN + Point2D(TileGeography.SIZE, TileGeography.SIZE * 2), 1.0, Direction.EAST)
+        val desiredDirection = GhostAI.desiredDirection(blinky, pacman)
+        assert(desiredDirection == Direction.EAST || desiredDirection == Direction.WEST)
       }
     }
   }
