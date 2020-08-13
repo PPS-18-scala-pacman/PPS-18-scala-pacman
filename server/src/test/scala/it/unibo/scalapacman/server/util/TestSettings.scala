f7a9b4ed509fc934d6b7cf784b272f1415f3b99f
MooDySmiles <diphlo.powa@gmail.com>
2020-08-13 21:10:36 +0200

    Fix server test

--- server/src/test/scala/it/unibo/scalapacman/server/util/TestSettings.scala
+++ server/src/test/scala/it/unibo/scalapacman/server/util/TestSettings.scala
@@ -1,8 +1,11 @@
 package it.unibo.scalapacman.server.util
 
+import java.util.concurrent.TimeUnit
+
 import scala.concurrent.duration.{DurationInt, FiniteDuration}
 
 object TestSettings {
 
   val askTestDuration: FiniteDuration = Settings.askDuration * 2
+  val waitTime: FiniteDuration = FiniteDuration(10, TimeUnit.SECONDS)// scalastyle:ignore magic.number
 }
