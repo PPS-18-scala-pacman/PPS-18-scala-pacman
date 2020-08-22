761f5f96430195469e59566ac26d97ecae7853d4
McGiogen <mcgiogen@hotmail.it>
2020-08-22 18:53:10 +0200

    Aggiunge ulteriori test a GameMovement

--- pacman-lib/src/test/scala/it/unibo/scalapacman/lib/engine/GameMovementTest.scala
+++ pacman-lib/src/test/scala/it/unibo/scalapacman/lib/engine/GameMovementTest.scala
@@ -6,6 +6,8 @@ import it.unibo.scalapacman.lib.model.Character.Pacman
 import it.unibo.scalapacman.lib.model.{Character, Direction, Map, Tile}
 import org.scalatest.wordspec.AnyWordSpec
 
+import scala.concurrent.duration.{FiniteDuration, MILLISECONDS}
+
 class GameMovementTest extends AnyWordSpec {
   val MAP_SIZE = 4
   val TIME_MS = 1
@@ -51,6 +53,16 @@ class GameMovementTest extends AnyWordSpec {
         val result: Character = move(character, TIME_MS, character.direction)
         assert(result.position.x == attended.x && result.position.y == attended.y && result.direction == character.direction)
       }
+      "go straight the most if it's not possible to reach the next center" in {
+        val character = Pacman(
+          TileGeography.center,
+          TileGeography.SIZE / 2,
+          Direction.EAST
+        )
+        val attended: Point2D = TileGeography.center + Point2D(TileGeography.SIZE * 0.5, 0)
+        val result: Character = move(character, TIME_MS, character.direction)
+        assert(result.position.x == attended.x && result.position.y == attended.y && result.direction == character.direction)
+      }
       "stop if it isn't possible" in {
         val character = Pacman(
           TileGeography.northGate + Point2D(0, TileGeography.SIZE * 2),
@@ -84,6 +96,49 @@ class GameMovementTest extends AnyWordSpec {
         val attended: Point2D = character.position
         val result: Character = move(character, TIME_MS, desiredDirection)
         assert(result.position.x == attended.x && result.position.y == attended.y && result.direction == desiredDirection)
+      }
+    }
+    "it want to move" should {
+      "be able to use FiniteDuration" in {
+        val character = Pacman(
+          TileGeography.center,
+          TileGeography.SIZE,
+          Direction.WEST
+        )
+        val attended: Point2D = TileGeography.center + Point2D(TileGeography.SIZE * 3, 0)
+        val result: Character = GameMovement.move(character, FiniteDuration(1, MILLISECONDS), Some(character.direction))
+        assert(result == move(character, TIME_MS, character.direction))
+      }
+      "be able to don't have a desired direction" in {
+        val character = Pacman(
+          TileGeography.center,
+          TileGeography.SIZE,
+          Direction.WEST
+        )
+        val attended: Point2D = TileGeography.center + Point2D(TileGeography.SIZE * 3, 0)
+        val result: Character = GameMovement.move(character, FiniteDuration(1, MILLISECONDS), None)
+        assert(result == move(character, TIME_MS, character.direction))
+      }
+      "not move" when {
+        "it is died" in {
+          val character = Pacman(
+            TileGeography.center,
+            TileGeography.SIZE,
+            Direction.WEST,
+            isDead = true
+          )
+          val result: Character = move(character, TIME_MS, character.direction)
+          assert(result.position == character.position)
+        }
+        "it has no time" in {
+          val character = Pacman(
+            TileGeography.center,
+            TileGeography.SIZE,
+            Direction.WEST
+          )
+          val result: Character = move(character, timeMs = 0, character.direction)
+          assert(result.position == character.position)
+        }
       }
     }
   }
