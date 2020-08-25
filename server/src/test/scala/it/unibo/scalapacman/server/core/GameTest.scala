f2740ecb9291ffddcc0d280cf5ff795265e9b488
luca <luca.maestri@studio.unibo.it>
2020-08-25 12:12:01 +0200

    Aggiunto test per attore Game riguardo alla gestione della registrazione di Player

--- server/src/test/scala/it/unibo/scalapacman/server/core/GameTest.scala
+++ server/src/test/scala/it/unibo/scalapacman/server/core/GameTest.scala
@@ -1,22 +1,69 @@
 package it.unibo.scalapacman.server.core
 
+import akka.actor.testkit.typed.TestException
 import akka.actor.testkit.typed.scaladsl.{ScalaTestWithActorTestKit, TestProbe}
 import akka.actor.typed.ActorRef
-import it.unibo.scalapacman.server.util.ConfLoader
+import akka.http.scaladsl.model.ws.Message
+import it.unibo.scalapacman.server.communication.ConnectionProtocol.ConnectionFailed
+import it.unibo.scalapacman.server.core.Game.{CloseCommand, RegisterPlayer}
+import it.unibo.scalapacman.server.core.Player.{PlayerRegistration, RegistrationAccepted, RegistrationRejected}
+import it.unibo.scalapacman.server.util.{ConfLoader, TestSettings}
+import org.scalatest.BeforeAndAfterEach
 import org.scalatest.wordspec.AnyWordSpecLike
 
-class GameTest extends ScalaTestWithActorTestKit(ConfLoader.config) with AnyWordSpecLike {
+class GameTest extends ScalaTestWithActorTestKit(ConfLoader.config) with AnyWordSpecLike with BeforeAndAfterEach {
 
   private var gameActor: ActorRef[Game.GameCommand] = _
+  private var fooProbe: TestProbe[Message] = _
 
-  override def beforeAll(): Unit = {
+  override def beforeEach(): Unit = {
     gameActor = spawn(Game("TestActorGame"))
+    fooProbe = createTestProbe[Message]()
   }
 
   "A Master actor" must {
 
+    "handle player registration" in {
+
+      val testProbe = createTestProbe[PlayerRegistration]()
+      val exMsg = "Test connessione caduta"
+
+      gameActor ! RegisterPlayer(testProbe.ref, fooProbe.ref)
+      testProbe.receiveMessage() match {
+        case RegistrationAccepted(messageHandler) =>
+          messageHandler ! ConnectionFailed(TestException(exMsg))
+        case _ => fail()
+      }
+
+      //Attendo che player elabori il messaggio ConnectionFailed
+      Thread.sleep(TestSettings.waitTime.toMillis)
+
+      gameActor ! RegisterPlayer(testProbe.ref, fooProbe.ref)
+      testProbe.receiveMessage() match {
+        case RegistrationAccepted(_) =>
+        case _ => fail()
+      }
+    }
+
+    "reject registration during game" in {
+
+      val testProbe = createTestProbe[PlayerRegistration]()
+
+      gameActor ! RegisterPlayer(testProbe.ref, fooProbe.ref)
+      testProbe.receiveMessage() match {
+        case RegistrationAccepted(_) =>
+        case _ => fail()
+      }
+
+      gameActor ! RegisterPlayer(testProbe.ref, fooProbe.ref)
+      testProbe.receiveMessage() match {
+        case RegistrationRejected(_) =>
+        case _ => fail()
+      }
+    }
+
     "is stoppable" in {
-      gameActor ! Game.CloseCommand()
+      gameActor ! CloseCommand()
       TestProbe().expectTerminated(gameActor)
     }
   }
