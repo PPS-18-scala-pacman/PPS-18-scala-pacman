cd96619653c7dd39885a2b6bc041a20ddd81f2e1
luca <luca.maestri@studio.unibo.it>
2020-11-06 18:19:31 +0100

    Modifiche Game per gestione partire con più giocatori

--- server/src/main/scala/it/unibo/scalapacman/server/core/Game.scala
+++ server/src/main/scala/it/unibo/scalapacman/server/core/Game.scala
@@ -4,9 +4,9 @@ import akka.actor.typed.{ActorRef, Behavior, ChildFailed, MailboxSelector, Termi
 import akka.actor.typed.receptionist.{Receptionist, ServiceKey}
 import akka.actor.typed.scaladsl.{ActorContext, Behaviors}
 import akka.http.scaladsl.model.ws.Message
-import it.unibo.scalapacman.common.GameCharacter
 import it.unibo.scalapacman.lib.model.GhostType
 import it.unibo.scalapacman.lib.model.GhostType.GhostType
+import it.unibo.scalapacman.lib.model.PlayerType.PlayerType
 import it.unibo.scalapacman.server.core.Engine.EngineCommand
 import it.unibo.scalapacman.server.core.Game.{CloseCommand, GameCommand, Model, RegisterPlayer, Setup}
 import it.unibo.scalapacman.server.core.Player.{PlayerCommand, PlayerRegistration, RegistrationRejected}
@@ -25,14 +25,18 @@ object Game {
   case class CloseCommand() extends GameCommand
   case class RegisterPlayer(replyTo: ActorRef[PlayerRegistration], source: ActorRef[Message]) extends GameCommand
 
+  private case class Player(ref: ActorRef[PlayerCommand], typePlayer: PlayerType)
+
   private case class Setup( id: String,
                             context: ActorContext[GameCommand],
-                            engine: ActorRef[EngineCommand])
+                            engine: ActorRef[EngineCommand],
+                            playersNumber: Int)
 
-  private case class Model( player: ActorRef[PlayerCommand],
-                            ghosts: Map[ActorRef[Engine.UpdateCommand],GhostType])
+  private case class Model( players: Set[Player],
+                            ghosts: Map[ActorRef[Engine.UpdateCommand], GhostType])
 
-  def apply(id: String, visible: Boolean = true): Behavior[GameCommand] =
+  def apply(id: String, playersNumber: Int = 1, visible: Boolean = true): Behavior[GameCommand] = {
+    require(playersNumber > 0 || playersNumber <= Settings.maxPlayersNumber, "Numero di giocatori errato")
     Behaviors.setup { context =>
 
       if(visible) {
@@ -42,17 +46,17 @@ object Game {
 
       // inizializzazione attori partecipanti
       val engine = context.spawn(Engine(id, Settings.levelDifficulty), "EngineActor")
-      val player = context.spawn(Player(id, engine), "PlayerActor")
 
-      val props  = MailboxSelector.fromConfig("ghost-mailbox")
-      val ghosts = GhostType.values.map( gt =>
+      val props = MailboxSelector.fromConfig("ghost-mailbox")
+      val ghosts = GhostType.values.map(gt =>
         context.spawn(GhostAct(id, engine, gt), s"${gt}Actor", props) -> gt
       ).toMap
 
-      (Set(engine, player) ++ ghosts.keySet).foreach(context.watch(_))
+      (Set(engine) ++ ghosts.keySet).foreach(context.watch(_))
 
-      new Game(Setup(id, context, engine)).start(Model(player, ghosts))
+      new Game(Setup(id, context, engine, playersNumber)).start(Model(Set(), ghosts))
     }
+  }
 
 }
 
@@ -66,7 +70,10 @@ private class Game(setup: Setup) {
       case CloseCommand() => close()
       case RegisterPlayer(replyTo, source) =>
         setup.context.log.info("RegisterPlayer ricevuto")
-        model.player ! Player.RegisterUser(replyTo, source)
+        val player = setup.context.spawn(Player(setup.id, setup.engine), s"PlayerActor")
+        player ! Player.RegisterUser(replyTo, source)
+
+        //TODO  if numPl == registerPlayer core else init
         prepareBehavior(coreRoutine, model)
     }
 
@@ -103,12 +110,13 @@ private class Game(setup: Setup) {
     recBe(model).receiveSignal {
       case (context, ChildFailed(act@setup.engine, _)) =>
         context.log.error(s"$act crashed")
-        Behaviors.stopped
+        Behaviors.stopped/*
       case (context, ChildFailed(act@model.player, _)) =>
+        //TODO togliere il player dalla set andare in initRout con numRegPl--
         context.log.error(s"$act stopped")
         setup.engine ! Engine.ActorRecovery(GameCharacter.PACMAN)
         val player = context.spawn(Player(setup.id, setup.engine), "PlayerActor")
-        prepareBehavior(initRoutine, model.copy(player = player))
+        prepareBehavior(initRoutine, model.copy(player = player))*/
       case (context, ChildFailed(act, _)) if act.isInstanceOf[ActorRef[Engine.UpdateCommand]] =>
         context.log.error(s"$act stopped")
         val ghostAct = act.asInstanceOf[ActorRef[Engine.UpdateCommand]]
