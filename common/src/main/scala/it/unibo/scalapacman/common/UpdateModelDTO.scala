083f21da1b1256ed134a7a424d9b4e3fe457cdb4
luca <luca.maestri@studio.unibo.it>
2020-11-10 20:30:58 +0100

    Modificato dto comunicazione client-server

--- common/src/main/scala/it/unibo/scalapacman/common/UpdateModelDTO.scala
+++ common/src/main/scala/it/unibo/scalapacman/common/UpdateModelDTO.scala
@@ -5,18 +5,25 @@ import it.unibo.scalapacman.lib.model.Character.{Ghost, Pacman}
 import it.unibo.scalapacman.lib.model.GhostType.{BLINKY, CLYDE, INKY, PINKY}
 import it.unibo.scalapacman.lib.model.Map.MapIndexes
 import it.unibo.scalapacman.lib.model.{Dot, Fruit, GameState}
+import it.unibo.scalapacman.lib.model.PlayerType.{PLAYER_ONE, PLAYER_TWO, PLAYER_THREE, PLAYER_FOUR}
 
 
-case class GameEntityDTO(gameCharacterHolder: GameCharacterHolder, position: Point2D, speed: Double, isDead: Boolean, dir: DirectionHolder) {
+case class GameEntityDTO(gameCharacterHolder: GameCharacterHolder, id: Option[String], position: Point2D,
+                         speed: Double, isDead: Boolean, dir: DirectionHolder) {
   def toGhost: Option[Ghost] = gameCharacterHolder.gameChar match {
-    case GameCharacter.INKY   => Some(Ghost(INKY, position, speed, dir.direction, isDead))
+    case GameCharacter.INKY   => Some(Ghost(INKY,   position, speed, dir.direction, isDead))
     case GameCharacter.BLINKY => Some(Ghost(BLINKY, position, speed, dir.direction, isDead))
-    case GameCharacter.CLYDE  => Some(Ghost(CLYDE, position, speed, dir.direction, isDead))
-    case GameCharacter.PINKY  => Some(Ghost(PINKY, position, speed, dir.direction, isDead))
+    case GameCharacter.CLYDE  => Some(Ghost(CLYDE,  position, speed, dir.direction, isDead))
+    case GameCharacter.PINKY  => Some(Ghost(PINKY,  position, speed, dir.direction, isDead))
     case _ => None
   }
-  def toPacman: Option[Pacman] =
-    if(gameCharacterHolder.gameChar == GameCharacter.PACMAN) Some(Pacman(position, speed, dir.direction, isDead)) else None
+  def toPacman: Option[Pacman] = gameCharacterHolder.gameChar match {
+    case GameCharacter.PLAYER_ONE   => Some(Pacman(position, speed, dir.direction, isDead, PLAYER_ONE))
+    case GameCharacter.PLAYER_TWO   => Some(Pacman(position, speed, dir.direction, isDead, PLAYER_TWO))
+    case GameCharacter.PLAYER_THREE => Some(Pacman(position, speed, dir.direction, isDead, PLAYER_THREE))
+    case GameCharacter.PLAYER_FOUR  => Some(Pacman(position, speed, dir.direction, isDead, PLAYER_FOUR))
+    case _ => None
+  }
 }
 
 case class DotDTO(dotHolder: DotHolder, pos: MapIndexes)
