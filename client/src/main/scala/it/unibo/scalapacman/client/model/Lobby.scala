32dc5c5dc598886400641213d17ee6ddff8762ea
MooDySmiles <diphlo.powa@gmail.com>
2020-11-28 15:44:27 +0100

    Aggiorna modelli Aggiunge deserializzatore custom per Participant Aggiunge enums per eventType canale SSE

--- client/src/main/scala/it/unibo/scalapacman/client/model/Lobby.scala
+++ client/src/main/scala/it/unibo/scalapacman/client/model/Lobby.scala
@@ -1,18 +1,40 @@
 package it.unibo.scalapacman.client.model
 
-import spray.json.{DefaultJsonProtocol, JsonFormat}
+import it.unibo.scalapacman.lib.model.PacmanType
+import spray.json.{DefaultJsonProtocol, JsNumber, JsObject, JsString, JsValue, JsonFormat, RootJsonFormat, deserializationError}
 
-case class Lobby(id: Int, description: String, size: Int, participants: List[Participant]) {
+case class Lobby(id: Int, description: String, size: Int, hostUsername: String, participants: List[Participant]) {
   override def toString: String = s"$description - (${participants.size}/$size)"
 }
 
-case class Participant(username: String, host: Boolean, pacmanType: Int, lobbyId: Int) {
+case class Participant(username: String, pacmanType: PacmanType.PacmanType, lobbyId: Int) {
   override def toString: String = username
 }
 
 object LobbyJsonProtocol extends DefaultJsonProtocol {
 
-  implicit def participantFormat: JsonFormat[Participant] = jsonFormat4(Participant)
+  implicit object ParticipantJsonFormat extends RootJsonFormat[Participant] {
+    def write(p: Participant): JsValue = JsObject(
+      "lobbyId" -> JsNumber(p.lobbyId),
+      "pacmanType" -> JsNumber(PacmanType.playerTypeValToIndex(p.pacmanType)),
+      "username" -> JsString(p.username),
+    )
 
-  implicit def lobbyFormat: JsonFormat[Lobby] = jsonFormat4(Lobby)
+    def read(value: JsValue): Participant = value.asJsObject.getFields("lobbyId", "pacmanType", "username") match {
+      case Seq(JsNumber(lobbyId), JsNumber(pacmanType), JsString(username)) =>
+        Participant(username, PacmanType.indexToPlayerTypeVal(pacmanType.toInt), lobbyId.toInt)
+      case _ => deserializationError("Participant expected")
+    }
+  }
+
+  implicit def lobbyFormat: JsonFormat[Lobby] = jsonFormat5(Lobby)
+}
+
+object LobbySSEEventType extends Enumeration {
+  val LOBBY_CREATE: LobbySSEEventType.Value = Value("Lobby/Create")
+  val LOBBY_DELETE: LobbySSEEventType.Value = Value("Lobby/Delete")
+  val LOBBY_UPDATE: LobbySSEEventType.Value = Value("Lobby/Update")
+  val PARTICIPANT_CREATE: LobbySSEEventType.Value = Value("Participant/Create")
+  val PARTICIPANT_DELETE: LobbySSEEventType.Value = Value("Participant/Delete")
+  val PARTICIPANT_UPDATE: LobbySSEEventType.Value = Value("Participant/Update")
 }
