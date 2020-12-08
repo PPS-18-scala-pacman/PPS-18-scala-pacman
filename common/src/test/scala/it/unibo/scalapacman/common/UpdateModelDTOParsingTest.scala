483d7c0a1046474ade836ca568b03fc2f1bcecf9
MooDySmiles <diphlo.powa@gmail.com>
2020-12-08 16:09:00 +0100

    Fix test

--- common/src/test/scala/it/unibo/scalapacman/common/UpdateModelDTOParsingTest.scala
+++ common/src/test/scala/it/unibo/scalapacman/common/UpdateModelDTOParsingTest.scala
@@ -45,7 +45,7 @@ class UpdateModelDTOParsingTest extends AnyWordSpec with BeforeAndAfterAll with 
       "dir\":{\"direction\":\"NORTH\"}}],\"state\":{\"score\":2,\"ghostInFear\":false,\"pacmanEmpowered\":false,\"" +
       "levelStateHolder\":{\"levelState\":\"ONGOING\"}},\"dots\":[{\"dotHolder\":{\"dot\":\"SMALL_DOT\"},\"pos\":[5,6]}," +
       "{\"dotHolder\":{\"dot\":\"SMALL_DOT\"},\"pos\":[6,6]},{\"dotHolder\":{\"dot\":\"SMALL_DOT\"},\"pos\":[7,6]},{\"" +
-      "dotHolder\":{\"dot\":\"SMALL_DOT\"},\"pos\":[8,6]}],\"fruit\":{\"fruitHolder\":{\"fruit\":\"APPLE\"},\"pos\":[9,9]}}"
+      "dotHolder\":{\"dot\":\"SMALL_DOT\"},\"pos\":[8,6]}],\"fruit\":{\"fruitHolder\":{\"fruit\":\"APPLE\"},\"pos\":[9,9]},\"paused\":false}"
   }
 
   "An UpdateModel" must {
